fb5a5423893a9988b7bde81087c73352f4df2bee
PJ Fanning <pjfanning@users.noreply.github.com>
2023-02-14 10:37:19 +0100

    stray comma

--- project/MetaInfLicenseNoticeCopy.scala
+++ project/MetaInfLicenseNoticeCopy.scala
@@ -19,7 +19,7 @@ object MetaInfLicenseNoticeCopy {
     Seq(
       resourceGenerators += copyFileToMetaInf(resourceManaged, "LICENSE"),
       resourceGenerators += copyFileToMetaInf(resourceManaged, "NOTICE"),
-      resourceGenerators += copyFileToMetaInf(resourceManaged, "DISCLAIMER"),))
+      resourceGenerators += copyFileToMetaInf(resourceManaged, "DISCLAIMER")))
 
   def copyFileToMetaInf(dir: SettingKey[File], fileName: String) = Def.task[Seq[File]] {
     val fromFile = (LocalRootProject / baseDirectory).value / fileName

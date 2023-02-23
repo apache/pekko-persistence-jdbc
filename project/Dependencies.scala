f31dc6329aa17d1c502c3d0e70a44a6ff9342cd3
PJ Fanning <pjfanning@users.noreply.github.com>
2023-02-23 20:58:08 +0100

    use latest pekko snapshot

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,3 +1,4 @@
+import PekkoDependency.pekkoVersion
 import sbt._
 
 object Dependencies {
@@ -5,8 +6,6 @@ object Dependencies {
   val Scala212 = "2.12.16"
   val Scala213 = "2.13.8"
   val ScalaVersions = Seq(Scala212, Scala213)
-
-  val PekkoVersion = "0.0.0+26546-767209a8-SNAPSHOT"
 
   val SlickVersion = "3.3.3"
   val ScalaTestVersion = "3.2.10"
@@ -18,14 +17,14 @@ object Dependencies {
     "com.microsoft.sqlserver" % "mssql-jdbc" % "7.4.1.jre8")
 
   val Libraries: Seq[ModuleID] = Seq(
-    "org.apache.pekko" %% "pekko-persistence-query" % PekkoVersion,
+    "org.apache.pekko" %% "pekko-persistence-query" % pekkoVersion,
     "com.typesafe.slick" %% "slick" % SlickVersion,
     "com.typesafe.slick" %% "slick-hikaricp" % SlickVersion,
     "ch.qos.logback" % "logback-classic" % "1.2.10" % Test,
-    "org.apache.pekko" %% "pekko-slf4j" % PekkoVersion % Test,
-    "org.apache.pekko" %% "pekko-persistence-tck" % PekkoVersion % Test,
-    "org.apache.pekko" %% "pekko-stream-testkit" % PekkoVersion % Test,
-    "org.apache.pekko" %% "pekko-testkit" % PekkoVersion % Test,
+    "org.apache.pekko" %% "pekko-slf4j" % pekkoVersion % Test,
+    "org.apache.pekko" %% "pekko-persistence-tck" % pekkoVersion % Test,
+    "org.apache.pekko" %% "pekko-stream-testkit" % pekkoVersion % Test,
+    "org.apache.pekko" %% "pekko-testkit" % pekkoVersion % Test,
     "org.scalatest" %% "scalatest" % ScalaTestVersion % Test) ++ JdbcDrivers.map(_ % Test)
 
   val Migration: Seq[ModuleID] = Seq(

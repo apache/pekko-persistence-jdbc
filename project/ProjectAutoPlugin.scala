9ad8298f66fa542d92496bfb5b90230337469e99
PJ Fanning <pjfanning@users.noreply.github.com>
2023-03-04 15:20:58 +0100

    add apache headers and add CI check

--- project/ProjectAutoPlugin.scala
+++ project/ProjectAutoPlugin.scala
@@ -1,3 +1,12 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, derived from Akka.
+ */
+
 import com.geirsson.CiReleasePlugin
 import de.heikoseeberger.sbtheader.HeaderPlugin
 import de.heikoseeberger.sbtheader.HeaderPlugin.autoImport.{ headerLicense, HeaderLicense }

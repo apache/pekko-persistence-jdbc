d0938c27e058eb20f5f1641ec0aef615a043f9d7
PJ Fanning <pjfanning@users.noreply.github.com>
2025-03-13 10:04:21 +0100

    update function name to match pekko API change (#271)

--- integration-test/src/test/scala/org/apache/pekko/persistence/jdbc/integration/CurrentLastKnownSequenceNumberByPersistenceIdTest.scala
+++ integration-test/src/test/scala/org/apache/pekko/persistence/jdbc/integration/CurrentLastKnownSequenceNumberByPersistenceIdTest.scala
@@ -10,7 +10,7 @@
 package org.apache.pekko.persistence.jdbc.integration
 
 import org.apache.pekko.persistence.jdbc.query.{
-  CurrentLastKnownSequenceNumberByPersistenceIdTest,
+  CurrentLastSequenceNumberByPersistenceIdTest,
   MysqlCleaner,
   OracleCleaner,
   PostgresCleaner,
@@ -19,18 +19,18 @@ import org.apache.pekko.persistence.jdbc.query.{
 
 // Note: these tests use the shared-db configs, the test for all (so not only current) events use the regular db config
 
-class PostgresScalaCurrentLastKnownSequenceNumberByPersistenceIdTest
-    extends CurrentLastKnownSequenceNumberByPersistenceIdTest("postgres-shared-db-application.conf")
+class PostgresScalaCurrentLastSequenceNumberByPersistenceIdTest
+    extends CurrentLastSequenceNumberByPersistenceIdTest("postgres-shared-db-application.conf")
     with PostgresCleaner
 
-class MySQLScalaCurrentLastKnownSequenceNumberByPersistenceIdTest
-    extends CurrentLastKnownSequenceNumberByPersistenceIdTest("mysql-shared-db-application.conf")
+class MySQLScalaCurrentLastSequenceNumberByPersistenceIdTest
+    extends CurrentLastSequenceNumberByPersistenceIdTest("mysql-shared-db-application.conf")
     with MysqlCleaner
 
-class OracleScalaCurrentLastKnownSequenceNumberByPersistenceIdTest
-    extends CurrentLastKnownSequenceNumberByPersistenceIdTest("oracle-shared-db-application.conf")
+class OracleScalaCurrentLastSequenceNumberByPersistenceIdTest
+    extends CurrentLastSequenceNumberByPersistenceIdTest("oracle-shared-db-application.conf")
     with OracleCleaner
 
-class SqlServerScalaCurrentLastKnownSequenceNumberByPersistenceIdTest
-    extends CurrentLastKnownSequenceNumberByPersistenceIdTest("sqlserver-shared-db-application.conf")
+class SqlServerScalaCurrentLastSequenceNumberByPersistenceIdTest
+    extends CurrentLastSequenceNumberByPersistenceIdTest("sqlserver-shared-db-application.conf")
     with SqlServerCleaner

f04b05f12fdbdf92f10a455949b09bbb40a7f7b8
Marco Paggioro <marco.paggioro@gmail.com>
2025-11-20 13:37:01 +0100

    Implement Durable State handling with MySQL (#174) (#366)
    * Implement Durable State handling with MySQL (#174)
    
    * Aligned SQL to other queries
    
    * Add (selectable) explicit select for global offset in MySQL Durable State implementation and tests
    
    * Less DB locking: from REPLACE to INSERT
    
    * Fix tests: `StateSpecBase` was not dropping DB schema on `afterAll`
    
    * Fix typo + Parametric column + Tests
    
    * Rename `offset` column to `current_offset` in MySQL durable state schema to avoid conflicts
    
    ---------
    
    Co-authored-by: PJ Fanning <pjfanning@users.noreply.github.com>

--- core/src/main/resources/schema/mysql/mysql-drop-schema.sql
+++ core/src/main/resources/schema/mysql/mysql-drop-schema.sql
@@ -1,3 +1,5 @@
 DROP TABLE IF EXISTS event_tag;
 DROP TABLE IF EXISTS event_journal;
 DROP TABLE IF EXISTS snapshot;
+DROP TABLE IF EXISTS durable_state;
+DROP TABLE IF EXISTS durable_state_global_offset;

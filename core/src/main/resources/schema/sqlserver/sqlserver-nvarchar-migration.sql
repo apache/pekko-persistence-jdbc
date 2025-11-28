b65d161d7fa94390498ee2d2717ecaf84230720a
PJ Fanning <pjfanning@users.noreply.github.com>
2025-11-29 00:06:09 +0100

    Update sqlserver-nvarchar-migration.sql

--- core/src/main/resources/schema/sqlserver/sqlserver-nvarchar-migration.sql
+++ core/src/main/resources/schema/sqlserver/sqlserver-nvarchar-migration.sql
@@ -3,21 +3,37 @@
 -- see https://github.com/apache/pekko-persistence-jdbc/pull/382
 
 ALTER TABLE event_journal ALTER COLUMN
-  "persistence_id" NVARCHAR(255),
-  "writer" NVARCHAR(255),
-  "adapter_manifest" NVARCHAR(MAX),
-  "event_ser_manifest" NVARCHAR(MAX),
+  "persistence_id" NVARCHAR(255);
+
+ALTER TABLE event_journal ALTER COLUMN
+  "writer" NVARCHAR(255);
+
+ALTER TABLE event_journal ALTER COLUMN
+  "adapter_manifest" NVARCHAR(MAX);
+
+ALTER TABLE event_journal ALTER COLUMN
+  "event_ser_manifest" NVARCHAR(MAX);
+
+ALTER TABLE event_journal ALTER COLUMN
   "meta_ser_manifest" NVARCHAR(MAX);
 
 ALTER TABLE "snapshot" ALTER COLUMN
   "tag" NVARCHAR(255);
 
 ALTER TABLE "snapshot" ALTER COLUMN
-  "persistence_id" NVARCHAR(255),
-  "snapshot_ser_manifest" NVARCHAR(255),
+  "persistence_id" NVARCHAR(255);
+
+ALTER TABLE "snapshot" ALTER COLUMN
+  "snapshot_ser_manifest" NVARCHAR(255);
+
+ALTER TABLE "snapshot" ALTER COLUMN
   "meta_ser_manifest" NVARCHAR(255);
 
 ALTER TABLE durable_state ALTER COLUMN
-  "persistence_id" NVARCHAR(255),
-  "state_serial_manifest" NVARCHAR(MAX),
+  "persistence_id" NVARCHAR(255);
+
+ALTER TABLE durable_state ALTER COLUMN
+  "state_serial_manifest" NVARCHAR(MAX);
+
+ALTER TABLE durable_state ALTER COLUMN
   "tag" NVARCHAR(255);

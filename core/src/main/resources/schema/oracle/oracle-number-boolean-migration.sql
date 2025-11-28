b17219a73547010e9d697ca5ea23226b516bfb5f
PJ Fanning <pjfanning@users.noreply.github.com>
2025-11-29 00:00:05 +0100

    fix name and add checks

--- core/src/main/resources/schema/oracle/oracle-number-boolean-migration.sql
+++ core/src/main/resources/schema/oracle/oracle-number-boolean-migration.sql
@@ -2,5 +2,5 @@
 
 -- see https://github.com/apache/pekko-persistence-jdbc/pull/323
 
-ALTER TABLE EVENT_JOURNAL MODIFY "deleted" NUMBER(1)
+ALTER TABLE EVENT_JOURNAL MODIFY DELETED NUMBER(1) DEFAULT 0 NOT NULL check (DELETED in (0, 1))
 /
--- core/src/main/resources/schema/oracle/oracle-number-boolean-schema-legacy-migration.sql
+++ core/src/main/resources/schema/oracle/oracle-number-boolean-schema-legacy-migration.sql
@@ -2,5 +2,5 @@
 
 -- see https://github.com/apache/pekko-persistence-jdbc/pull/323
 
-ALTER TABLE "journal" MODIFY "deleted" NUMBER(1)
+ALTER TABLE "journal" MODIFY "deleted" NUMBER(1) check ("deleted" in (0,1)) NOT NULL
 /

3c879b1239d3aabcd6d3ea70b1b6761b1414f43b
PJ Fanning <pjfanning@users.noreply.github.com>
2025-11-28 19:58:13 +0100

    Update sqlserver-nvarchar-migration.sql

--- core/src/main/resources/schema/sqlserver/sqlserver-nvarchar-migration.sql
+++ core/src/main/resources/schema/sqlserver/sqlserver-nvarchar-migration.sql
@@ -3,13 +3,18 @@
 -- see https://github.com/apache/pekko-persistence-jdbc/pull/382
 
 ALTER TABLE event_journal ALTER COLUMN
-  "persistence_id" NVARCHAR(255)
+  "persistence_id" NVARCHAR(255),
+  "writer" NVARCHAR(255),
   "adapter_manifest" NVARCHAR(MAX),
   "event_ser_manifest" NVARCHAR(MAX),
   "meta_ser_manifest" NVARCHAR(MAX);
 
 ALTER TABLE "snapshot" ALTER COLUMN
+  "tag" NVARCHAR(255);
+
+ALTER TABLE "snapshot" ALTER COLUMN
   "persistence_id" NVARCHAR(255),
+  "snapshot_ser_manifest" NVARCHAR(255),
   "meta_ser_manifest" NVARCHAR(255);
 
 

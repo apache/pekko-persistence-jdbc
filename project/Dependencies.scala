c6f3de1fb85526561d9955d72a22391697e779a6
Scala Steward <scala_steward@virtuslab.com>
2026-01-31 18:03:27 +0000

    Update mysql-connector-j to 9.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
   val JdbcDrivers = Seq(
     "org.postgresql" % "postgresql" % "42.7.9",
     "com.h2database" % "h2" % "2.4.240",
-    "com.mysql" % "mysql-connector-j" % "9.5.0",
+    "com.mysql" % "mysql-connector-j" % "9.6.0",
     "org.mariadb.jdbc" % "mariadb-java-client" % "3.5.7",
     "com.microsoft.sqlserver" % "mssql-jdbc" % "13.2.1.jre11",
     "com.oracle.database.jdbc" % "ojdbc8" % "23.26.0.0.0")

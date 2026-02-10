0b8ec0696cf70acff1a0a25bb57445cb250e8cff
Scala Steward <scala_steward@virtuslab.com>
2026-02-10 15:40:07 +0000

    Update ojdbc8 to 23.26.1.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
     "com.mysql" % "mysql-connector-j" % "9.6.0",
     "org.mariadb.jdbc" % "mariadb-java-client" % "3.5.7",
     "com.microsoft.sqlserver" % "mssql-jdbc" % "13.2.1.jre11",
-    "com.oracle.database.jdbc" % "ojdbc8" % "23.26.0.0.0")
+    "com.oracle.database.jdbc" % "ojdbc8" % "23.26.1.0.0")
 
   val Libraries: Seq[ModuleID] = Seq(
     "com.typesafe.slick" %% "slick" % SlickVersion,

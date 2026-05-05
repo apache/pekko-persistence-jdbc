b363cd0eed1c1221a8e32d81cca11d2c7ae06946
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-05-05 17:44:09 +0200

    Update typesafe:config to 1.4.8 (#497)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
     "org.scalatest" %% "scalatest" % ScalaTestVersion % Test) ++ JdbcDrivers.map(_ % Test)
 
   val Migration: Seq[ModuleID] = Seq(
-    "com.typesafe" % "config" % "1.4.7",
+    "com.typesafe" % "config" % "1.4.8",
     "ch.qos.logback" % "logback-classic" % LogbackVersion,
     "org.scalatest" %% "scalatest" % ScalaTestVersion % Test) ++ JdbcDrivers.map(_ % Provided)
 }

74f0439e2e5cb568908bb0f5916b513b6d244374
Scala Steward <scala_steward@virtuslab.com>
2026-08-11 13:54:51 +0000

    Update logback-classic to 1.6.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
 
   val PekkoVersion = PekkoCoreDependency.version
 
-  val LogbackVersion = "1.6.1"
+  val LogbackVersion = "1.6.2"
 
   val SlickVersion = "3.6.1"
   val SlickDocVersion = SlickVersion

fc2737c99f7e8522f339c79836e851ef3f228813
PJ Fanning <pjfanning@users.noreply.github.com>
2026-09-04 16:14:57 +0100

    Update Scala versions in Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,9 +11,9 @@ import sbt._
 
 object Dependencies {
   // Keep in sync with .github CI build
-  val Scala213 = "3.9.0"
+  val Scala213 = "2.13.18"
   val Scala3 = "3.3.8"
-  val Scala3Next = "3.8.4"
+  val Scala3Next = "3.9.0"
   val ScalaVersions = Seq(Scala213, Scala3, Scala3Next)
 
   val PekkoVersion = PekkoCoreDependency.version
